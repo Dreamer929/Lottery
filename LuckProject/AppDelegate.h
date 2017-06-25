0fd0ecd0ceecea32029fdcc1ed0e4656eb41371d
moxi <moxi@192.168.0.104>
2017-06-25 17:21:25 +0800

    Initial Commit

---
+++ LuckProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuckProject
+//
+//  Created by moxi on 2017/6/25.
+//  Copyright © 2017年 moxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
